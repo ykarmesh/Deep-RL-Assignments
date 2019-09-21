07d8235e87d1cba64f0b87ed571fe5d8850d4c8b
Karmesh Yadav <ykarmesh@gmail.com>
2019-09-21 00:53:08 -0400

    First Commit

---
+++ Assignment2/hw2-VI-PI-DQN/Q3-3-DQN/DQN_Implementation.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+import keras, tensorflow as tf, numpy as npy, gym, sys, copy, argparse
+
+class QNetwork():
+
+	# This class essentially defines the network architecture. 
+	# The network should take in state of the world as an input, 
+	# and output Q values of the actions available to the agent as the output. 
+
+	def __init__(self, environment_name):
+		# Define your network architecture here. It is also a good idea to define any training operations 
+		# and optimizers here, initialize your variables, or alternately compile your model here.  
+		pass
+
+	def save_model_weights(self, suffix):
+		# Helper function to save your model / weights. 
+		pass
+
+	def load_model(self, model_file):
+		# Helper function to load an existing model.
+		# e.g.: torch.save(self.model.state_dict(), model_file)
+		pass
+
+	def load_model_weights(self,weight_file):
+		# Helper funciton to load model weights. 
+		# e.g.: self.model.load_state_dict(torch.load(model_file))
+		pass
+
+
+class Replay_Memory():
+
+	def __init__(self, memory_size=50000, burn_in=10000):
+
+		# The memory essentially stores transitions recorder from the agent
+		# taking actions in the environment.
+
+		# Burn in episodes define the number of episodes that are written into the memory from the 
+		# randomly initialized agent. Memory size is the maximum size after which old elements in the memory are replaced. 
+		# A simple (if not the most efficient) was to implement the memory is as a list of transitions. 
+		
+		# Hint: you might find this useful:
+		# 		collections.deque(maxlen=memory_size)
+	 	pass
+
+	def sample_batch(self, batch_size=32):
+		# This function returns a batch of randomly sampled transitions - i.e. state, action, reward, next state, terminal flag tuples. 
+		# You will feed this to your model to train.
+		pass
+
+	def append(self, transition):
+		# Appends transition to the memory. 	
+		pass
+
+class DQN_Agent():
+
+	# In this class, we will implement functions to do the following. 
+	# (1) Create an instance of the Q Network class.
+	# (2) Create a function that constructs a policy from the Q values predicted by the Q Network. 
+	#		(a) Epsilon Greedy Policy.
+	# 		(b) Greedy Policy. 
+	# (3) Create a function to train the Q Network, by interacting with the environment.
+	# (4) Create a function to test the Q Network's performance on the environment.
+	# (5) Create a function for Experience Replay.
+	
+	def __init__(self, environment_name, render=False):
+
+		# Create an instance of the network itself, as well as the memory. 
+		# Here is also a good place to set environmental parameters,
+		# as well as training parameters - number of episodes / iterations, etc. 
+		pass 
+
+	def epsilon_greedy_policy(self, q_values):
+		# Creating epsilon greedy probabilities to sample from.             
+		pass
+
+	def greedy_policy(self, q_values):
+		# Creating greedy policy for test time. 
+		pass 
+
+	def train(self):
+		# In this function, we will train our network. 
+		# If training without experience replay_memory, then you will interact with the environment 
+		# in this function, while also updating your network parameters. 
+
+		# When use replay memory, you should interact with environment here, and store these 
+		# transitions to memory, while also updating your model.
+		pass
+
+	def test(self, model_file=None):
+		# Evaluate the performance of your agent over 100 episodes, by calculating cummulative rewards for the 100 episodes.
+		# Here you need to interact with the environment, irrespective of whether you are using a memory. 
+
+	def burn_in_memory():
+		# Initialize your replay memory with a burn_in number of episodes / transitions. 
+		pass
+		
+		
+
+def parse_arguments():
+	parser = argparse.ArgumentParser(description='Deep Q Network Argument Parser')
+	parser.add_argument('--env',dest='env',type=str)
+	parser.add_argument('--render',dest='render',type=int,default=0)
+	parser.add_argument('--train',dest='train',type=int,default=1)
+	parser.add_argument('--model',dest='model_file',type=str)
+	return parser.parse_args()
+
+def main(args):
+
+	args = parse_arguments()
+	environment_name = args.env
+
+	# Setting the session to allow growth, so it doesn't allocate all GPU memory. 
+	gpu_ops = tf.GPUOptions(allow_growth=True)
+	config = tf.ConfigProto(gpu_options=gpu_ops)
+	sess = tf.Session(config=config)
+
+	# Setting this as the default tensorflow session. 
+	keras.backend.tensorflow_backend.set_session(sess)
+
+	# You want to create an instance of the DQN_Agent class here, and then train / test it. 
+
+if __name__ == '__main__':
+	main(sys.argv)
+
