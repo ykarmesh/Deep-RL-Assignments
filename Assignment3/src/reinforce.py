68bf200cd37df10f66268beee86423b7a8b65692
Heethesh Vhavle <heethesh@gmail.com>
2019-10-12 17:39:05 -0400

    Updating dist with categorical

--- Assignment3/src/reinforce.py
+++ Assignment3/src/reinforce.py
@@ -31,7 +31,7 @@ class Model(torch.nn.Module):
         x = F.relu(self.linear1(x))
         x = F.relu(self.linear2(x))
         x = F.relu(self.linear3(x))
-        x = F.log_softmax(self.output(x), dim=1)
+        x = F.softmax(self.output(x), dim=1)
         return x
 
 
@@ -146,7 +146,11 @@ class Reinforce():
             self.summary_writer.add_scalar('test/rewards_std', rewards_std, epoch)
         self.model.train()
 
-    def generate_batch(self, epoch):
+    def generate_batch(self, epoch, single_episode=False):
+        if single_episode:
+            returns, log_probs = self.generate_episode()
+            return (returns * log_probs).mean().neg()
+
         while True:
             # Generate episode data.
             returns, log_probs = self.generate_episode()
@@ -196,10 +200,14 @@ class Reinforce():
 
             # Sample action from the log probabilities.
             # Deterministic at test time, stochastic at train time.
-            if test and self.args.det_eval: action = torch.argmax(action_probs)
-            else: action = torch.argmax(torch.distributions.Multinomial(logits=action_probs).sample())
+            if test and self.args.det_eval:
+                action = torch.argmax(action_probs)
+                log_probs.append(torch.log(torch.max(action_probs)))
+            else:
+                sampler = torch.distributions.Categorical(probs=action_probs)
+                action = sampler.sample()
+                log_probs.append(sampler.log_prob(action))
             actions.append(action)
-            log_probs.append(action_probs[action])
 
             # Run simulation with current action to get new state and reward.
             if render: monitor.render()
